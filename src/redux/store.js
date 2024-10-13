7a009ea6377054be74c8ff0fbcfbc67435e6d6e8
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-10-13 16:57:49 +0200

    added libs and removed persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,40 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import storage from "redux-persist/lib/storage";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-  whitelist: ['items'],
-};
-
-const persistedReducer = persistReducer(persistConfig, contactsReducer);
-
 const store = configureStore({
-  // https://redux-toolkit.js.org/usage/usage-guide#working-with-non-serializable-data // documentation 
   reducer: {
-    contacts: persistedReducer,
+    contacts: contactsReducer,
     filters: filtersReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
 
-const persistor = persistStore(store);
-export { store, persistor };
+export default store;
